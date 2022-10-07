c87456d262613a35820ec6b9d2581eae558d8afc
Beniamin <benjanekko@gmail.com>
2022-10-07 18:27:40 +0100

    files_modified

---
+++ AGH_course_exercises/III_sem/05.10.2022/printing_prime_numbers.py
@@ -0,0 +1,23 @@
+from math import sqrt
+from sys import argv
+
+def prime(n):
+    if n == 2 or n == 5:
+        return True
+    if n % 2 == 0 or n == 1:
+        return False
+    i = 3
+    while i < sqrt(n):
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+    
+if __name__=="__main__":
+    for number in argv:
+        try:
+            number = int(number)
+            if prime(number):
+                print(number)
+        except:
+            continue
--- Hackerrank_challenges/prime.py
+++ Hackerrank_challenges/prime.py
@@ -4,7 +4,7 @@ def prime(n):
     if n == 2 or n == 5:
         print("Prime")
         return
-    if n % 2 or n == 1:
+    if n % 2 == 0 or n == 1:
         print("Not prime")
         return
     i = 3
