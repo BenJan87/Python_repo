3a34492103bb2d489268e8e096289e9bd6544ef6
Beniamin <benjanekko@gmail.com>
2022-10-24 00:23:48 +0100

    string_manipualtion_finished

--- AGH_course_exercises/III_sem/26.10.2022/string_manipulation.py
+++ AGH_course_exercises/III_sem/26.10.2022/string_manipulation.py
@@ -1,12 +1,16 @@
 import argparse, re, os
 
-def printing_file(file):
-    file = open(file, "r")
-    for el in file.readlines():
-        print(el, end="")
-    file.close()
 
-def concation(given_file, end_char):
+def printing_file(file):
+    file1 = open(file, "r")
+    print(f"\nopening: {file}\n")
+    for el in file1.readlines():
+        print(el, end="")
+    print("\n"+"-"*40)
+    file1.close()
+
+
+def concatenation(given_file, end_char):
     tmp_file = open('tmp_file.txt', 'w+')
 
     try:
@@ -17,18 +21,19 @@ def concation(given_file, end_char):
         os.remove("tmp_file.txt")
         return False
 
-    
     lines = given_file.readlines()
     tmp_string = ""
+
     if end_char in "\\*+?.|[]\{\}^$":
         pattern = rf"\{end_char}$"
     else:
         pattern = rf"{end_char}$"
 
+    last = lines[-1]
     for line in lines:
-        last = lines[-1]
         if line is not last:
             line = line[:-1]
+
         if re.search(pattern, line):
             tmp_string += line[:-1]
         else:
@@ -39,41 +44,111 @@ def concation(given_file, end_char):
                 tmp_file.write(tmp_string)
             tmp_string = ""
     
+    print("After concatenation:\n")
     tmp_file.close()
     printing_file("tmp_file.txt")
     tmp_file.close()
-    os.remove("tmp_file.txt")
+    return True
     
 
-
 def spaces(file):
-    pass
+    tmp_file = open("tmp_file_spaces.txt", 'w+')
+    given_file = open(file, 'r')
+    lines = given_file.readlines()
+    last = lines[-1]
+    
+    for line in lines:
+        result_string = ""
+        result_arr = re.findall(r'^\s+\S+', line)
+        if result_arr:
+            result_arr += re.findall(r'\S+', line)[1:]
+        else:
+            result_arr = re.findall(r'\S+', line)
+
+        for only_word in result_arr:
+            result_string += only_word
+        
+        if line is not last:
+            tmp_file.write(result_string+"\n")
+        else:
+            tmp_file.write(result_string)
+
+    print("After removing spaces:\n")
+    tmp_file.close()
+    printing_file("tmp_file_spaces.txt")
+    tmp_file.close()
 
 
 def leading_spaces(file):
-    pass
+    tmp_file = open('tmp_file_leading.txt', 'w+')
+    given_file = open(file, 'r')
+    lines = given_file.readlines()
+    last = lines[-1]
+
+    for line in lines:
+        reg_object = re.search(r'^\s+', line)
+        if reg_object:
+            new_line = line[reg_object.end():]
+        else:
+            new_line = line
+        
+        if line is not last:
+            tmp_file.write(new_line)
+        else:
+            tmp_file.write(new_line) 
+            
+    print("After removing leading spaces:\n")
+    tmp_file.close()
+    printing_file("tmp_file_leading.txt")
+    tmp_file.close()  
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--files", help="input file", required=True, nargs='+')
+    parser.add_argument("-f", "--files", help="input file",
+                        nargs='+', required=True)
     parser.add_argument('-l', '--leading-spaces', help='skip the spaces except the leading spaces',
-                        nargs='+')
-    parser.add_argument('-s', '--spaces', help='skipt all of the spaces', nargs='+')
+                        action='store_true')
+    parser.add_argument('-s', '--spaces', help='skipt all of the spaces',
+                        action='store_true')
     parser.add_argument('-c', '--concat', help='converting the continutation of code into one line (ends with "\\")',
                         default="\\")
     
-
     try:
         args = parser.parse_args()
-        
     except:
         print("Specify file names and check the flags (-h or --help for descriptions)")
 
     files = args.files
+    basic_file = "tmp_file.txt"
+    try:
+        for file in files:
+            if not concatenation(file, args.concat[0]):
+                print(f"No such file as {file}\n")
+                continue
 
-    for file in files:
-        if not concation(file, args.concat[0]):
+            if args.leading_spaces:
+                leading_spaces(basic_file)
+                if args.spaces:
+                    spaces("tmp_file_leading.txt")
+                    os.rename("tmp_file_spaces.txt", file+"_changed")
+                    continue
+
+                os.rename("tmp_file_leading.txt", file+"_changed")
+                continue
+
+            if args.spaces:
+                spaces(basic_file)
+                os.rename("tmp_file_spaces.txt", file+"_changed")
+                continue
+
+            os.rename("tmp_file.txt", file+"_changed")
+    except(FileExistsError):
+        print("File with name 'file_name'_changed already exist\nRemove this file than run the script")
+
+    for file in [basic_file, "tmp_file_leading.txt", "tmp_file_spaces.txt"]:
+        try:
+            os.remove(file)
+        except:
             pass
-    
--- AGH_course_exercises/III_sem/26.10.2022/test_main.py
+++ AGH_course_exercises/III_sem/26.10.2022/test_main.py
@@ -1,6 +1,7 @@
 from errno import ESTALE
 import os
 import re
+from socket import SO_LINGER
 
 #how to find with re end of the line /? and
 def printing_file(file):
@@ -47,7 +48,36 @@ def concation(given_file, end_char):
     tmp_file.close()
     printing_file("tmp_file.txt")
     tmp_file.close()
+
+def spaces(file):
+    tmp_file = open('tmp_file_3.txt', 'w+')
+    given_file = open(file, 'r')
+    lines = given_file.readlines()
+    last = lines[-1]
+    
+    for line in lines:
+        result_string = ""
+        result_arr = re.findall(r'^\s+\S+', line)
+        if result_arr:
+            result_arr += re.findall(r'\S+', line)[1:]
+        else:
+            result_arr = re.findall(r'\S+', line)
+
+        for only_word in result_arr:
+            result_string += only_word
+        
+        if line is not last:
+            tmp_file.write(result_string+"\n")
+        else:
+            tmp_file.write(result_string)
+
+    print("After removing spaces")
+    tmp_file.close()
+    printing_file("tmp_file_3.txt")
+    tmp_file.close()
     
 if __name__ == "__main__":
     concation("C:\\Users\\benja\\OneDrive\\Pulpit\\Studia_AGH\\git_repos\\Python_repo\\AGH_course_exercises\\III_sem\\26.10.2022\\file.txt",
-             "|")
+            "\\")
+    spaces("C:\\Users\\benja\\OneDrive\\Pulpit\\Studia_AGH\\git_repos\\Python_repo\\AGH_course_exercises\\III_sem\\26.10.2022\\tmp_file.txt")
+     
