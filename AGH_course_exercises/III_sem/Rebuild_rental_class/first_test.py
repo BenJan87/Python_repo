314e232a4af993ac40aa175e35a85fbb9bb69586
Beniamin <benjanekko@gmail.com>
2022-11-14 11:54:47 +0100

    rental_bike_rebuild_finished

--- AGH_course_exercises/III_sem/Rebuild_rental_class/first_test.py
+++
@@ -1,7 +0,0 @@
-def client(argument):
-    argument += 1
-    def __str__():
-        return argument
-
-# client(3)
-# print(client(3))
--- AGH_course_exercises/III_sem/Rebuild_rental_class/main_test.py
+++
@@ -1,9 +0,0 @@
-class Ben():
-    def __init__(self):
-        self.age = 20
-
-    def __str__(self):
-        return f"{self.age}"
-
-obj = Ben()
-print(obj)
