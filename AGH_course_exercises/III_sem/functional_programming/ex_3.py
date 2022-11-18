aa8aa28657d8a6529705d65ab7deb7d227614714
Beniamin <benjanekko@gmail.com>
2022-11-18 19:09:42 +0100

    ex_3

---
+++ AGH_course_exercises/III_sem/functional_programming/ex_3.py
@@ -0,0 +1,7 @@
+# one
+from sys import argv;print(list(filter(lambda x: x % 2 == 0, [int(el) for sublist in [line.strip().split() for el in argv[1:] for line in open(el, 'r')] for el in sublist])))
+
+# For better understand:
+# from sys import argv
+# res = list(filter(lambda x: x % 2 == 0, [int(el) for sublist in [line.strip().split() for el in argv[1:] for line in open(el, 'r')] for el in sublist]))
+# print(res)
